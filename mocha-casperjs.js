c60d3cdeb49212a671220401829e9b2437e93f68
dankasierertmc <d.kasierer@tmcbonds.com>
2017-03-24 09:44:29 -0400

    Update mocha-casperjs.js

--- mocha-casperjs.js
+++ mocha-casperjs.js
@@ -115,6 +115,7 @@ module.exports = function (Mocha, casper, utils) {
             fn.call(this, (function(mycount) {
               return function() {
                 console.log("DKDONE " + mycount + ". 1 of 3");
+                console.log(fn);
                 done();
               };
             })(instanceCount));
